e5d1bee648bc1a63e4e8d32fb5819eb19c2a2727
Federico Lencina <federiconahuellencina@gmail.com>
2021-07-30 15:25:28 -0300

    Se agrega marco a la lista

--- Script/index.js
+++ Script/index.js
@@ -1,5 +1,43 @@
-var botonAgregar = document.getElementById("btn-buscar");
+var botonAgregar = document.getElementById("btn-agregar");
 
-function buscarPelicula( nombre ){
-    var 
+function agregarElemento( nombre ){
+    var caja = document.createElement("div");
+
+    var herramienta = document.createElement("h3");
+
+    var eliminar = document.createElement("button");
+
+    caja.classList.add("contenedor");
+
+    herramienta.innerText= nombre;
+
+    eliminar.innerText="Retirar";
+    eliminar.classList.add("btn-eliminar");
+    retirarHerramienta(eliminar);
+
+    caja.appendChild( herramienta );
+    caja.appendChild( eliminar );
+    
+
+    var lista = document.getElementById("listaHerramientas");
+    lista.appendChild(caja);
+    
+}
+
+
+botonAgregar.addEventListener("click", function(){
+    var input = document.getElementById("herramienta");
+
+    if(input.value!="" && input.value!=null){
+        agregarElemento ( input.value );
+    }
+    
+    input.value = "";
+
+})
+
+function retirarHerramienta(boton){
+    boton.addEventListener("click", function(){
+        boton.parentElement.remove();
+    })
 }
